95465b4a68e24796cf95ea149de5800c0ce0cefd
gcg <cgge@best-inc.com>
2019-05-28 10:03:05 +0800

    finish one-hundred-four

--- src/leetcode/Test.java
+++ src/leetcode/Test.java
@@ -7,18 +7,17 @@ package leetcode;
 public class Test {
 
     public static void main(String[] args) {
-        OneHundredOne oneHundredOne = new OneHundredOne();
+        OneHundredFour oneHundredFour = new OneHundredFour();
 
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(2);
-        root.left.left = new TreeNode(3);
-        root.left.right = new TreeNode(4);
-        root.right.left = new TreeNode(4);
-        root.right.right = new TreeNode(3);
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        root.right.left.left = new TreeNode(15);
 
 
-        System.out.println(oneHundredOne.isSymmetric(root));
+        System.out.println(oneHundredFour.maxDepth(root));
     }
 
 }
