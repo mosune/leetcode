c4f88a754db8a903feb08bbe49064c7fcdab014d
gcg <cgge@best-inc.com>
2019-06-04 10:52:51 +0800

    finish one-hundred-eleven

---
+++ src/leetcode/OneHundredEleven.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+/**
+ *
+ * 给定一个二叉树，找出其最小深度。
+ *
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例:
+ *
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回它的最小深度  2.
+ *
+ * @author gcg
+ * @create 2019-06-04 10:14
+ **/
+public class OneHundredEleven {
+
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null) return minDepth(root.right) + 1;
+        if (root.right == null) return minDepth(root.left) + 1;
+        int left = minDepth(root.left) + 1;
+        int right = minDepth(root.right) + 1;
+        return left < right ? left : right;
+    }
+
+}
--- src/leetcode/Test.java
+++ src/leetcode/Test.java
@@ -1,8 +1,5 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author gcg
  * @create 2019-04-26 22:27
@@ -10,22 +7,15 @@ import java.util.List;
 public class Test {
 
     public static void main(String[] args) {
-        OneHundredTen oneHundredTen = new OneHundredTen();
+        OneHundredEleven oneHundredEleven = new OneHundredEleven();
 
         TreeNode root = new TreeNode(3);
-        root.left = new TreeNode(9);
-        root.right = new TreeNode(20);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
         root.left.left = new TreeNode(3);
-        root.right.right = new TreeNode(3);
-        root.left.left.left = new TreeNode(15);
-        root.right.right.right = new TreeNode(7);
+        root.left.right = new TreeNode(3);
 
-//        System.out.println(oneHundredTen.isBalanced(root));
-
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        System.out.println(1 + ":" + list.toString());
+        System.out.println(oneHundredEleven.minDepth(root));
     }
 
 }
