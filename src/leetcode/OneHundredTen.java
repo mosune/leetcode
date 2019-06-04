4151101af5a86d016c9dcf220754c3c84bb859f0
gcg <cgge@best-inc.com>
2019-06-04 10:06:32 +0800

    finish one-hundred-ten

---
+++ src/leetcode/OneHundredTen.java
@@ -0,0 +1,66 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ *
+ * 本题中，一棵高度平衡二叉树定义为：
+ *
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+ *
+ * 示例 1:
+ *
+ * 给定二叉树 [3,9,20,null,null,15,7]
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回 true 。
+ *
+ * 示例 2:
+ *
+ * 给定二叉树 [1,2,2,3,3,null,null,4,4]
+ *
+ *        1
+ *       / \
+ *      2   2
+ *     / \
+ *    3   3
+ *   / \
+ *  4   4
+ * 返回 false 。
+ *
+ * @author gcg
+ * @create 2019-06-03 12:24
+ **/
+public class OneHundredTen {
+
+    public boolean isBalanced(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode treeNode = queue.poll();
+            if (treeNode == null) continue;
+            queue.offer(treeNode.left);
+            queue.offer(treeNode.right);
+            int left = getDepth(treeNode.left);
+            int right = getDepth(treeNode.right);
+            int s = left - right;
+            if (s > 1 || s < -1) return false;
+        }
+        return true;
+    }
+
+    private int getDepth(TreeNode node) {
+        if (node == null) return 0;
+        int left = getDepth(node.left) + 1;
+        int right = getDepth(node.right) + 1;
+        return left > right ? left :right;
+    }
+
+}
--- src/leetcode/Test.java
+++ src/leetcode/Test.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author gcg
  * @create 2019-04-26 22:27
@@ -7,12 +10,22 @@ package leetcode;
 public class Test {
 
     public static void main(String[] args) {
-        OneHundredEight oneHundredEight = new OneHundredEight();
+        OneHundredTen oneHundredTen = new OneHundredTen();
 
-        int[] nums = {-10, -3, 0, 5, 9};
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.left.left = new TreeNode(3);
+        root.right.right = new TreeNode(3);
+        root.left.left.left = new TreeNode(15);
+        root.right.right.right = new TreeNode(7);
 
-        TreeNode treeNode = oneHundredEight.sortedArrayToBST(nums);
-        System.out.println(((1 - 1) >> 1) + 1);
+//        System.out.println(oneHundredTen.isBalanced(root));
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        System.out.println(1 + ":" + list.toString());
     }
 
 }
