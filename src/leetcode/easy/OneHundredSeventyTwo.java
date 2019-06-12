0f56f79dbc0a8a072624e7b761b23169480b319f
gcg <cgge@best-inc.com>
2019-06-12 16:23:02 +0800

    finish one-hundred-seventy-two

---
+++ src/leetcode/easy/OneHundredSeventyTwo.java
@@ -0,0 +1,39 @@
+package leetcode.easy;
+
+/**
+ * 172. 阶乘后的零
+ *
+ * 给定一个整数 n，返回 n! 结果尾数中零的数量。
+ *
+ * 示例 1:
+ *
+ * 输入: 3
+ * 输出: 0
+ * 解释: 3! = 6, 尾数中没有零。
+ * 示例 2:
+ *
+ * 输入: 5
+ * 输出: 1
+ * 解释: 5! = 120, 尾数中有 1 个零.
+ * 说明: 你算法的时间复杂度应为 O(log n) 。
+ *
+ * @author gcg
+ * @create 2019-06-12 15:24
+ **/
+public class OneHundredSeventyTwo {
+
+    public int trailingZeroes(int n) {
+        int sum = 0;
+
+        long a = 5;
+
+        // 统计5的数量
+        while (n / a != 0) {
+            sum += n / a;
+            a *= 5;
+        }
+
+        return sum;
+    }
+
+}
