da6bf2f661b0c13da2f4d518fc0db442f313b92f
gcg <cgge@best-inc.com>
2019-07-31 15:42:18 +0800

    finish four-hundred-twelve

---
+++ src/leetcode/easy/FourHundredTwelve.java
@@ -0,0 +1,56 @@
+package leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 412. Fizz Buzz
+ *
+ * 写一个程序，输出从 1 到 n 数字的字符串表示。
+ *
+ * 1. 如果 n 是3的倍数，输出“Fizz”；
+ *
+ * 2. 如果 n 是5的倍数，输出“Buzz”；
+ *
+ * 3.如果 n 同时是3和5的倍数，输出 “FizzBuzz”。
+ *
+ * 示例：
+ *
+ * n = 15,
+ *
+ * 返回:
+ * [
+ *     "1",
+ *     "2",
+ *     "Fizz",
+ *     "4",
+ *     "Buzz",
+ *     "Fizz",
+ *     "7",
+ *     "8",
+ *     "Fizz",
+ *     "Buzz",
+ *     "11",
+ *     "Fizz",
+ *     "13",
+ *     "14",
+ *     "FizzBuzz"
+ * ]
+ *
+ * @author gcg
+ * @create 2019-07-31 14:59
+ **/
+public class FourHundredTwelve {
+
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) list.add("FizzBuzz");
+            else if (i % 3 == 0) list.add("Fizz");
+            else if (i % 5 == 0) list.add("Buzz");
+            else list.add(i + "");
+        }
+        return list;
+    }
+
+}
