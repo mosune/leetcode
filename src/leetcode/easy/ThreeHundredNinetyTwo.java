20c313709a7808148ee8c2dfffb4d98eacbcfe5a
gcg <cgge@best-inc.com>
2019-08-28 10:52:53 +0800

    finish three-hundred-ninety-two

---
+++ src/leetcode/easy/ThreeHundredNinetyTwo.java
@@ -0,0 +1,90 @@
+package leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 392. 判断子序列
+ *
+ * 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+ *
+ * 你可以认为 s 和 t 中仅包含英文小写字母。字符串 t 可能会很长（长度 ~= 500,000），而 s 是个短字符串（长度 <=100）。
+ *
+ * 字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+ *
+ * 示例 1:
+ * s = "abc", t = "ahbgdc"
+ *
+ * 返回 true.
+ *
+ * 示例 2:
+ * s = "axc", t = "ahbgdc"
+ *
+ * 返回 false.
+ *
+ * 后续挑战 :
+ *
+ * 如果有大量输入的 S，称作S1, S2, ... , Sk 其中 k >= 10亿，你需要依次检查它们是否为 T 的子序列。在这种情况下，你会怎样改变代码？
+ *
+ * 致谢:
+ *
+ * 特别感谢 @pbrother 添加此问题并且创建所有测试用例。
+ *
+ * @author gcg
+ * @create 2019-08-27 16:45
+ **/
+public class ThreeHundredNinetyTwo {
+
+    /**
+     * 双指针
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0) return true;
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        int a = 0, b = 0;
+        while (b < tChars.length) {
+            if (sChars[a] == tChars[b++]) a++;
+            if (a == s.length()) return true;
+        }
+        return false;
+    }
+
+    /**
+     * 后续挑战
+     * @param s
+     * @param t
+     * @return
+     */
+//    public boolean isSubsequence(String s, String t) {
+//        // 提前初始化
+//        List<Integer>[] data = new ArrayList[26];
+//        char[] chars = t.toCharArray();
+//        for (int i = 0; i < chars.length; i++) {
+//            if (data[chars[i] - 97] == null) data[chars[i] - 97] = new ArrayList<>();
+//            data[chars[i] - 97].add(i);
+//        }
+//
+//        // 处理每个序列
+//        int index = -1;
+//        char[] newData = s.toCharArray();
+//        for (int i = 0; i < newData.length; i++) {
+//            List<Integer> list = data[newData[i] - 97];
+//            if (list == null) return false;
+//            else {
+//                int min = t.length();
+//                for (Integer num : list) {
+//                    if (num < min && num > index) min = num;
+//                }
+//                if (min == t.length()) return false;
+//                else index = min;
+//            }
+//        }
+//
+//        return true;
+//    }
+
+}
