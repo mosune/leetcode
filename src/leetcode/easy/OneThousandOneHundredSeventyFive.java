dbdc46d85fa0cba210b46403c0183ca721458a3d
gcg <cgge@best-inc.com>
2020-03-23 13:01:33 +0800

    finish one-thousand-one-hundred-seventy-five

---
+++ src/leetcode/easy/OneThousandOneHundredSeventyFive.java
@@ -0,0 +1,55 @@
+package leetcode.easy;
+
+/**
+ * 1175. 质数排列
+ *
+ * 请你帮忙给从 1 到 n 的数设计排列方案，使得所有的「质数」都应该被放在「质数索引」（索引从 1 开始）上；你需要返回可能的方案总数。
+ *
+ * 让我们一起来回顾一下「质数」：质数一定是大于 1 的，并且不能用两个小于它的正整数的乘积来表示。
+ *
+ * 由于答案可能会很大，所以请你返回答案 模 mod 10^9 + 7 之后的结果即可。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：n = 5
+ * 输出：12
+ * 解释：举个例子，[1,2,5,4,3] 是一个有效的排列，但 [5,2,3,4,1] 不是，因为在第二种情况里质数 5 被错误地放在索引为 1 的位置上。
+ * 示例 2：
+ *
+ * 输入：n = 100
+ * 输出：682289015
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= n <= 100
+ *
+ * @author gcg
+ * @create 2020-03-23 11:29
+ **/
+public class OneThousandOneHundredSeventyFive {
+
+    public int numPrimeArrangements(int n) {
+        int[] nums = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            if (nums[i] == 1) continue;
+            int r = i * 2, index = 3;
+            while (r <= n) {
+                nums[r]++;
+                r = i * index++;
+            }
+        }
+        nums[1]++;
+
+        int a = 0, b = 0;
+        Long num = 1L;
+        for (int i = 1; i <= n; i++) {
+            if (nums[i] == 0) num = num * ++b % 1000000007;
+            else num = num * ++a % 1000000007;
+        }
+        return num.intValue();
+    }
+
+}
