cbd718069bfd48a6368595e7ac203c1bbe3aba90
gcg <cgge@best-inc.com>
2020-03-26 15:10:28 +0800

    change a little OneThousandTwoHundredThirtySeven.java

--- src/leetcode/easy/OneThousandTwoHundredThirtySeven.java
+++ src/leetcode/easy/OneThousandTwoHundredThirtySeven.java
@@ -72,7 +72,7 @@ public class OneThousandTwoHundredThirtySeven {
         return results;
     }
 
-    static class CustomFunction {
+    class CustomFunction {
         // Returns f(x, y) for any given positive integers x and y.
         // Note that f(x, y) is increasing with respect to both x and y.
         // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
