8db113a74dfb7f38bead14ad7759ba399bbb17b5
gcg <cgge@best-inc.com>
2020-05-28 11:23:11 +0800

    finish interview-fifty-five-I

---
+++ src/leetcode/easy/InterviewFiftyFiveI.java
@@ -0,0 +1,38 @@
+package leetcode.easy;
+
+import leetcode.commom.TreeNode;
+
+/**
+ * 面试题55 - I. 二叉树的深度
+ *
+ * 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+ *
+ * 例如：
+ *
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回它的最大深度 3 。
+ *
+ *  
+ *
+ * 提示：
+ *
+ * 节点总数 <= 10000
+ * 注意：本题与主站 104 题相同：https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+ *
+ * @author gcg
+ * @create 2020-05-28 11:12
+ **/
+public class InterviewFiftyFiveI {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+}
