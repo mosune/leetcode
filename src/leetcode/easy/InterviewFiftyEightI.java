10c7eb2cc55f92753de971ace1ca232680ca41a9
gcg <cgge@best-inc.com>
2020-05-29 11:15:11 +0800

    finish interview-fifty-eight-I

---
+++ src/leetcode/easy/InterviewFiftyEightI.java
@@ -0,0 +1,59 @@
+package leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 面试题58 - I. 翻转单词顺序
+ *
+ * 输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。为简单起见，标点符号和普通字母一样处理。例如输入字符串"I am a student. "，则输出"student. a am I"。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入: "the sky is blue"
+ * 输出: "blue is sky the"
+ * 示例 2：
+ *
+ * 输入: "  hello world!  "
+ * 输出: "world! hello"
+ * 解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * 示例 3：
+ *
+ * 输入: "a good   example"
+ * 输出: "example good a"
+ * 解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ *  
+ *
+ * 说明：
+ *
+ * 无空格字符构成一个单词。
+ * 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ * 注意：本题与主站 151 题相同：https://leetcode-cn.com/problems/reverse-words-in-a-string/
+ *
+ * 注意：此题对比原题有改动
+ *
+ * @author gcg
+ * @create 2020-05-29 11:01
+ **/
+public class InterviewFiftyEightI {
+
+    public String reverseWords(String s) {
+        String[] strings = s.split(" ");
+        List<String> list = new ArrayList<>();
+
+        for (String string : strings) {
+            if (!"".equals(string.trim())) list.add(string);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(list.size() - 1 - i)).append(" ");
+        }
+
+        return "".contentEquals(sb) ? "" : sb.substring(0, sb.length() - 1);
+    }
+
+}
